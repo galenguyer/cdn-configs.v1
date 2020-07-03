01bda8dccf01376796baa895a49f06c88639d6b7
Galen Guyer <git@galenguyer.com>
2020-07-03 00:02:34 -0400

    Store all hostnames in a file and loop over them

--- force-sync-cdn.sh
+++ force-sync-cdn.sh
@@ -1,15 +1,6 @@
-ssh root@cdn-galenguyer-01.westus2.cloudapp.azure.com mkdir -p /var/www/cdn.galenguyer.com/
-rsync -avz --delete -e ssh /data/nginx/cdn/ root@cdn-galenguyer-01.westus2.cloudapp.azure.com:/var/www/cdn.galenguyer.com/
-ssh root@cdn-galenguyer-01.westus2.cloudapp.azure.com chown www-data:www-data /var/www -R
-
-ssh root@cdn-galenguyer-01.eastus.cloudapp.azure.com mkdir -p /var/www/cdn.galenguyer.com/
-rsync -avz --delete -e ssh /data/nginx/cdn/ root@cdn-galenguyer-01.eastus.cloudapp.azure.com:/var/www/cdn.galenguyer.com/
-ssh root@cdn-galenguyer-01.eastus.cloudapp.azure.com chown www-data:www-data /var/www -R
-
-ssh root@cdn-galenguyer-01.uksouth.cloudapp.azure.com mkdir -p /var/www/cdn.galenguyer.com/
-rsync -avz --delete -e ssh /data/nginx/cdn/ root@cdn-galenguyer-01.uksouth.cloudapp.azure.com:/var/www/cdn.galenguyer.com/
-ssh root@cdn-galenguyer-01.uksouth.cloudapp.azure.com chown www-data:www-data /var/www -R
-
-ssh root@cdn-galenguyer-01.australiaeast.cloudapp.azure.com mkdir -p /var/www/cdn.galenguyer.com/
-rsync -avz --delete -e ssh /data/nginx/cdn/ root@cdn-galenguyer-01.australiaeast.cloudapp.azure.com:/var/www/cdn.galenguyer.com/
-ssh root@cdn-galenguyer-01.australiaeast.cloudapp.azure.com chown www-data:www-data /var/www -R
+for HOST in `cat hostnames`
+do
+	ssh "$HOST" mkdir -p /var/www/cdn.galenguyer.com/
+	rsync -avz --delete -e ssh /data/nginx/cdn/ "$HOST":/var/www/cdn.galenguyer.com/
+        ssh "$HOST" chown www-data:www-data /var/www -R
+done
