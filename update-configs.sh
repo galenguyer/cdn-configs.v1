9c8169fcd80e783e8f08e4e70fc76c8c3817c5e5
Galen Guyer <galen@galenguyer.com>
2020-08-15 02:05:16 -0400

    Update everything to use persist for nyoom

--- update-configs.sh
+++ update-configs.sh
@@ -1,7 +1,8 @@
 for HOST in `cat hostnames`
 do
 	echo running on $HOST
-	ssh -t "$HOST" mkdir -p /etc/nginx/
+	ssh -t "$HOST" mkdir -p /etc/{nginx,persist}
 	rsync -avz --delete -e ssh ./nginx/ "$HOST":/etc/nginx/
-        ssh -t "$HOST" systemctl restart nginx
+	rsync -avz --delete -e ssh ./persist/ "$HOST":/etc/persist/
+        ssh -t "$HOST" 'systemctl enable persist; systemctl restart nginx; systemctl restart persist'
 done
--- update-packages.sh
+++ update-packages.sh
@@ -6,7 +6,11 @@ do
         rm -rf /var/cache/snapd/
         apt-get autoremove --purge snapd gnome-software-plugin-snap update-notifier update-manager -y  -qq
         curl -sSL https://packages.galenguyer.com/bootstrap.sh | sh
-        apt-get install edgeinx-mainline-quiche -y -qq
+        apt-get install edgeinx-mainline-quiche rsync inotify-tools -y -qq
         apt-get upgrade -y -qq
+        cd /tmp
+        wget https://blog.jmdawson.co.uk/persist_0.3_all.deb --no-check-certificate
+        dpkg -i persist_0.3_all.deb
+        systemctl enable persist
     '"
 done
