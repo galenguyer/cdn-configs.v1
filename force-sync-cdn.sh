93ea1c483ce3f1d9dde36bc2b0ee308adda21974
Galen Guyer <galen@galenguyer.com>
2020-08-28 12:11:22 -0400

    sync to ramdisk as well

--- force-sync-cdn.sh
+++ force-sync-cdn.sh
@@ -2,5 +2,6 @@ for HOST in `cat hostnames`
 do
 	ssh "$HOST" mkdir -p /var/www/cdn.galenguyer.com/
 	rsync -avz --delete -e ssh /data/nginx/cdn/ "$HOST":/var/www/cdn.galenguyer.com/
+	rsync -avz --delete -e ssh /data/nginx/cdn/ "$HOST":/opt/ramdisk/cdn.galenguyer.com/
         ssh "$HOST" chown www-data:www-data /var/www -R
 done
--- sync-homepage.sh
+++ sync-homepage.sh
@@ -2,5 +2,6 @@ for HOST in `cat hostnames`
 do
 	ssh "$HOST" mkdir -p /var/www/galenguyer.com/
 	rsync -avz --delete -e ssh /root/galenguyer.github.io/_site/ "$HOST":/var/www/galenguyer.com/
+	rsync -avz --delete -e ssh /root/galenguyer.github.io/_site/ "$HOST":/opt/ramdisk/galenguyer.com/
         ssh "$HOST" chown www-data:www-data /var/www -R
 done
