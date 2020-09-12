1e2b52ee05aab024d3e54234a84adc81655e586b
Galen Guyer <galen@galenguyer.com>
2020-09-12 00:28:38 -0400

    update cdn file path

--- force-sync-cdn.sh
+++ force-sync-cdn.sh
@@ -1,6 +1,6 @@
 for HOST in `cat hostnames`
 do
 	ssh "$HOST" mkdir -p /var/www/cdn.galenguyer.com/
-	rsync -avz --delete -e ssh /data/nginx/cdn/ "$HOST":/var/www/cdn.galenguyer.com/
+	rsync -avz --delete -e ssh /var/www/cdn.galenguyer.com/ "$HOST":/var/www/cdn.galenguyer.com/
         ssh "$HOST" chown www-data:www-data /var/www -R
 done
