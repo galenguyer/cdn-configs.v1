f5391cbbfb54d026304eb4c5c6f37ab7e8065ded
Galen Guyer <galen@galenguyer.com>
2020-09-19 21:04:38 +0000

    Add pretty output to aggregate-logs script

--- aggregate-logs.sh
+++ aggregate-logs.sh
@@ -1,3 +1,24 @@
+# equivalent to set -Eeu -o pipefail
+set -o errexit -o errtrace -o nounset -o pipefail
+
+function error {
+	echo "[$(tput setaf 1)X$(tput setaf 7)] $*"
+}
+function ok {
+	echo "[$(tput setaf 2)+$(tput setaf 7)] $*"
+}
+function info {
+	echo "[$(tput setaf 4)i$(tput setaf 7)] $*"
+}
+function cleanup {
+        tput setaf 7
+}
+trap cleanup EXIT
+function onerr {
+	error "$(caller): ${BASH_COMMAND}"
+}
+trap onerr ERR
+
 DATE="$(date +%Y-%m-%d)"
 
 # create folder for logs
@@ -5,7 +26,8 @@ mkdir -p "logs/nginx/$DATE"
 
 for HOST in `cat hostnames`
 do
-	echo running on $HOST
-	ssh -t "$HOST" 'cat /var/log/nginx/access.log' > "logs/nginx/$DATE/$HOST.log"
-	ssh -t "$HOST" 'cat /var/log/nginx/access.json' > "logs/nginx/$DATE/$HOST.json"
+	info "running on $HOST"
+	ssh -qt "$HOST" 'cat /var/log/nginx/access.log' > "logs/nginx/$DATE/$HOST.log"
+	ssh -qt "$HOST" 'cat /var/log/nginx/access.json' > "logs/nginx/$DATE/$HOST.json"
+	ok "retrieved logs from $HOST"
 done
